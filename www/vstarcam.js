78327eac44243e0fe8cbc64392a9ebeae2f05083
vdonkey <1467700782@qq.com>
2017-01-15 16:33:58 +0800

    Update vstarcam.js

--- www/vstarcam.js
+++ www/vstarcam.js
@@ -2,6 +2,7 @@ var exec = require('cordova/exec');
 
 module.exports = {
 	videostream: function(cameraId, user, pass, onSuccess, onError){
+		console.log('videostream-params:'+cameraId+','+ user + ',' + pass);
 		exec(onSuccess, onError, "Vstarcam", "videostream", [cameraId, user, pass]);
 	}
 };
